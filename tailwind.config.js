0c8fb49dad9c705ae9116bd642a47e6047f8c1a3
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-20 15:13:47 +0300

    Add tailwind-css configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    // container: {
+    //   center: true,
+    //   padding: '7rem',
+    // },
+    extend: {},
+  },
+  plugins: [],
+}
