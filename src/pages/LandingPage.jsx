73db2127642627e2d247a088f971e263299c1d1f
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-21 08:55:25 +0300

    Style and create the landing page

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav3 from '../components/Nav3'
+
+function LandingPage() {
+  return (
+    <div className='background'>
+ <Nav3/>
+ <div className=''>
+    <h1 className='text-white text-3xl '>Where will you go next</h1>
+ </div>
+     </div>
+
+  )
+}
+
+export default LandingPage
