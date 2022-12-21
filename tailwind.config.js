c0ea42a27b15cf3f174c2d273a32c6b6211da608
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-21 08:51:46 +0300

    Add responsiveness configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,15 @@
 module.exports = {
   content: ["./src/components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    // container: {
-    //   center: true,
-    //   padding: '7rem',
-    // },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
