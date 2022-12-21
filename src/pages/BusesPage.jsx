068743d3bece84027dff4d462241ea3791874de6
Joseph Kariuki  Macharia <joseph.macharia@student.moringaschool.com>
2022-12-21 16:37:08 +0300

    create available  bus page

---
+++ src/pages/BusesPage.jsx
@@ -0,0 +1,16 @@
+import BusCard from "../components/BusCard";
+import NavBar2 from "../components/NavBar2";
+
+function BusPages() {
+  return (
+    <>
+      <NavBar2 />
+      <BusCard />
+      <BusCard />
+      <BusCard />
+      
+    </>
+  );
+}
+
+export default BusPages;
