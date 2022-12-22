471207a9b93bcc66e668f0752882884a6879e8be
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-22 13:00:12 +0300

    Add components to the app file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar1 from './components/NavBar1';
 import NavBar2 from './components/NavBar2';
 import LandingPage from './pages/LandingPage';
+import LogInPage from './pages/LogInPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
   {/* <NavBar1/> */}
   {/* <NavBar2/> */}
   <LandingPage/>
+  {/* <LogInPage/> */}
     </div>
   );
 }
