1c844a363c52012b0b96251a418bce2dceb371f5
Joseph Kariuki  Macharia <joseph.macharia@student.moringaschool.com>
2022-12-23 20:12:40 +0300

    add dropdown

--- src/components/BusCard.jsx
+++ src/components/BusCard.jsx
@@ -1,10 +1,6 @@
 import React, { useState, useRef } from "react";
 
 function BusCard() {
-
-
-
-  
   const [visible, setVisible] = useState(false);
   const pageRef = useRef(null);
   const handleClick = () => {
@@ -13,13 +9,13 @@ function BusCard() {
   };
   return (
     <>
-      <div className=" flex flex-row justify-around bg-zinc-100 mt-6 shadow-2xl m-0 ">
+      <div className=" flex flex-row justify-around shadow-lg  width">
         {/* div holding image */}
         <div className=" flex flex-col justify-evenly  ">
           <img
             src="https://images.unsplash.com/photo-1544620347-c4fd4a3d5957?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YnVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
             alt="bus"
-            className="object-contain   max-h-48 p-4"
+            className="object-contain   max-h-32 p-4"
           />
           <h5 className="text-center">b</h5>
         </div>
@@ -53,10 +49,11 @@ function BusCard() {
           </button>
         </div>
       </div>
+      {/* drop down */}
       <div
-        className="fl  m-0"
+        className="flex p-12 m-0 justify-center"
         ref={pageRef}
-        style={{ display: visible ? "block" : "none" }}
+        style={{ display: visible ? "flex" : "none" }}
       >
         This is the page content
         <img
