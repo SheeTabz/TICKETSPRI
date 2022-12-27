677bc74d88f8bb3fc5e0c30c3804a8cba07fdbdc
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-27 13:43:47 +0300

    Add components to display in app file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
-import NavBar1 from './components/NavBar1';
-import NavBar2 from './components/NavBar2';
-import LandingPage from './pages/LandingPage';
-import LogInPage from './pages/LogInPage';
+import DashboardTemp from './components/DashboardTemp';
+
+
 
 function App() {
+
   return (
-    <div className="App">
-  {/* <NavBar1/> */}
-  {/* <NavBar2/> */}
-  <LandingPage/>
-  {/* <LogInPage/> */}
-    </div>
+   <div>
+<DashboardTemp/>
+</div>
   );
 }
 
