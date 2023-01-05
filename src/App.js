17bb39e2855cc94399004de0605a49b50f5d944c
SheeTabz <wmuriithitabitha@gmail.com>
2023-01-05 11:46:38 +0300

    Add components to app file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import DashboardTemp from './components/DashboardTemp';
 import SaccoLogin from './components/SaccoLogin';
 import SaccoSignup from './components/SaccoSignup';
 import SaccoLogiPage from './pages/SaccoLogiPage';
-
+import CarListPage from './pages/CarListPage';
+import EditForm from './components/EditForm';
+import LandingPage from './pages/LandingPage';
 
 
 function App() {
@@ -12,7 +14,10 @@ function App() {
    <div>
 {/* <SaccoLogin/> */}
 {/* <SaccoSignup/> */}
-<SaccoLogiPage/>
+{/* <SaccoLogiPage/> */}
+{/* <CarListPage/> */}
+{/* <EditForm/> */}
+<LandingPage/>
 </div>
   );
 }
