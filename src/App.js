5ae061103d2d3c1767fdcce4c5be3bd71102cd55
rodgersochieng <sirrodgers21@gmail.com>
2023-01-09 12:08:47 +0300

    custom changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 // import './App.css';
 // import DashboardTemp from "./components/DashboardTemp";
-// import Passenger from "./components/Passengers/Passenger";
-// import Profile from "./components/Passengers/Profile";
+import Passenger from "./components/Passengers/Passenger";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "jquery/dist/jquery.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
-import Payment from "./components/Passengers/Payment";
+// import Payment from "./components/Passengers/Payment";
 function App() {
   return (
     <>
-      {/* <Passenger /> */}
-      <Payment />
+      <Passenger />
     </>
   );
 }
