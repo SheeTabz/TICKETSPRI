5c0c285b55c8856fac18516c563753a00b470ced
rodgersochieng <sirrodgers21@gmail.com>
2023-01-11 09:26:34 +0300

    confirmation page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 // import './App.css';
 // import DashboardTemp from "./components/DashboardTemp";
-import Passenger from "./components/Passengers/Passenger";
+// import Passenger from "./components/Passengers/Passenger";
+// import Payment from "./components/Passengers/Payment";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "jquery/dist/jquery.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
-// import Payment from "./components/Passengers/Payment";
+// import { PDFDownloadLink } from "@react-pdf/renderer";
+
+// import PDFFile from "./components/Passengers/PDFFile";
+
+// import Confirmation from "./components/Passengers/Confirmation";
 function App() {
   return (
     <>
-      <Passenger />
+      {/* <Passenger /> */}
+      {/* <Confirmation /> */}
     </>
   );
 }
