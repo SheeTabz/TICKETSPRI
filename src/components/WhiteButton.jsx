227980c50c47a3292a76b959ec0aae8be5bc5a3e
SheeTabz <wmuriithitabitha@gmail.com>
2023-01-11 23:10:50 +0300

    button responsiveness

--- src/components/WhiteButton.jsx
+++ src/components/WhiteButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function WhiteButton() {
   return (
     <>
-    <button className='px-5 py-2 border-2 border-black rounded-md text-cyan-500 bg-white border-transparent'>Sign up </button>
+    <button className='md:px-5 md:py-2  px-2  py-2 border-2 border-black rounded-md text-cyan-500 bg-white border-transparent'>Sign up </button>
     </>
   )
 }
