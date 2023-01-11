4823fbfa03612c196ecab3bd9fe4ee213c910982
SheeTabz <wmuriithitabitha@gmail.com>
2023-01-11 11:53:08 +0300

    Add link component from react router

--- src/components/Nav3.jsx
+++ src/components/Nav3.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import {nav1} from './data'
 import TransButton from './TransButton'
 import WhiteButton from './WhiteButton'
@@ -8,14 +9,14 @@ function Nav3() {
     <div className='flex items-center md:container md:flex-row md:m-auto justify-between flex-col w-full '>
     <h1 className='font-bold lg:text-3xl text-2xl'>TICKETSPRI</h1>
     <div className='flex items-center md:space-x-5  space-x-2 md:w-fit w-full '>
-        <ul className='flex md:space-x-5 md:w-fit  w-full justify-around space-x-2'>
+        <ul className='flex md:space-x-5 md:w-fit  w-full justify-around space-x-2 items-center'>
         {nav1.map((data, index) => {
-        return <li key={index}>{data.name}</li>
+        return <Link to={data.path} className="text-white font-semibold no-underline"><li key={index}>{data.name}</li></Link> 
         })}
         </ul>
         <div className='flex flex-row space-x-5 hidden md:block'>
-            <TransButton/>
-            <WhiteButton/>
+             {/* <TransButton/>  */}
+             <Link to="/login"><WhiteButton/></Link>
         </div>
     </div>
     </div>
