05c1eb03444c51553bdf351f482f45a09e6a6986
SheeTabz <wmuriithitabitha@gmail.com>
2023-01-12 20:32:21 +0300

    Add link to home in the sacco nav bar

--- src/components/RegisteredNavBar.jsx
+++ src/components/RegisteredNavBar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import {IoPersonCircle }from  'react-icons/io5'
+import { Link } from 'react-router-dom'
 function RegisteredNavBar() {
   return (
   
 
  <div className='bg-gradient-to-r from-cyan-500 to-blue-600 h-[10vh]  text-white font-semibold flex '>
  <div className='w-full flex items-center lg:mx-[100px] justify-between md:mx-[20px] sm:mx-2'>
- <h1 className='font-bold md:text-3xl text-xl'>TICKETSPRI</h1>
+ <h1 className='font-bold md:text-3xl text-xl '>
+  <Link to="/" className='text-white no-underline'>
+  TICKETSPRI
+  </Link></h1>
+  
  <div className='flex items-center space-x-5 '>
      <ul className='flex space-x-5 '>
      <li className='md:text-3xl text-xl'>Easy coach</li>
