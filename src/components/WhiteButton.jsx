52592eb1c910f418824b9b0e629a096e1e357f90
SheeTabz <wmuriithitabitha@gmail.com>
2023-01-14 14:15:55 +0300

    Addd link from router dom to the button

--- src/components/WhiteButton.jsx
+++ src/components/WhiteButton.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function WhiteButton() {
   return (
     <>
+    <Link to={'/login'}>
     <button className='md:px-5 md:py-2  px-2  py-2 border-2 border-black rounded-md text-cyan-500 bg-white border-transparent'>Sign in </button>
+    </Link>
     </>
   )
 }
