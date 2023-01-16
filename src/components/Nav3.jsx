8bedd55927f7a04267e7157ae7d56172e99fce80
SheeTabz <wmuriithitabitha@gmail.com>
2023-01-16 18:59:30 +0300

    Design the logged inand logged out states of the avbar

--- src/components/Nav3.jsx
+++ src/components/Nav3.jsx
@@ -4,15 +4,9 @@ import {nav1} from './data'
 import TransButton from './TransButton'
 import WhiteButton from './WhiteButton'
 import {IoPersonCircle }from  'react-icons/io5'
-function Nav3({user, setUser}) {
-  function handleClick(){
+import LoggedinNav from './LoggedinNav'
+function Nav3({user, setUser, initials}) {
 
-    fetch("/customer/logout",{
-      method: "DELETE"
-    })
-    .then(resp => resp.json())
-    .then(setUser(null))
-      }
 
   return (
     <div className='h-[10vh] flex text-white font-semibold'>
@@ -21,15 +15,13 @@ function Nav3({user, setUser}) {
     <div className='flex items-center md:space-x-5  space-x-2 md:w-fit w-full '>
         <ul className='flex md:space-x-5 md:w-fit  w-full justify-around space-x-2'>
         {nav1.map((data, index) => {
-        return <Link to={data.path} className="text-white font-semibold no-underline"><li key={index}>{data.name}</li></Link> 
+        return <Link to={data.path} className="text-white font-semibold no-underline "><li key={index}>{data.name}</li></Link> 
         })}
         </ul>
         <div className='flex flex-row space-x-5 md:block'>
            
-            {user?  
-            (<div className='flex flex-row space-x-5'>
-              <button className='md:px-5 md:py-2  px-2 border-2 border-black rounded-md border-white ' onClick={handleClick}>Log out </button>
-            <span className='md:text-4xl text-3xl'><IoPersonCircle/></span></div>)
+            {user  ?  
+          <LoggedinNav  setUser={setUser} initials={initials}/>
             :<WhiteButton/>}
            
         </div>
