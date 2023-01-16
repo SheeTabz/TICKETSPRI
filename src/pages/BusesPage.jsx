f338aa9771aef711e0b07a1a02d27924c464e97d
Joseph Kariuki  Macharia <joseph.macharia@student.moringaschool.com>
2023-01-16 09:03:24 +0300

    connect page with backend

--- src/pages/BusesPage.jsx
+++ src/pages/BusesPage.jsx
@@ -1,14 +1,24 @@
+import { useState, useEffect } from "react";
 import BusCard from "../components/BusCard";
 import Footer from "../components/Footer";
 import NavBar2 from "../components/NavBar2";
 
 function BusPages() {
+  const [buses, setBuses] = useState([]);
+  useEffect(() => {
+    fetch("vehicles")
+      .then((response) => response.json())
+      .then((data) => setBuses(data));
+  }, []);
+  console.log(buses);
   return (
     <>
       <NavBar2 />
       <div className="space-y-8">
-        <BusCard />
-        <BusCard />
+        {buses.map((bus) => (
+          <BusCard key={bus.id} bus={bus.vehicle_name} image={bus.image} depature={bus.departure_time} arrival={bus.arrival_time} seats={bus.no_of_seats} price={bus.route.Price} ></BusCard>
+        ))}
+       
       </div>
       <Footer />
     </>
