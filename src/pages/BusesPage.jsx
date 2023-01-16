bbc2f5db096ff8f6d380a23199477fe31e9a6510
SheeTabz <wmuriithitabitha@gmail.com>
2023-01-16 19:03:14 +0300

    Pass props

--- src/pages/BusesPage.jsx
+++ src/pages/BusesPage.jsx
@@ -2,10 +2,10 @@ import BusCard from "../components/BusCard";
 import Footer from "../components/Footer";
 import NavBar2 from "../components/NavBar2";
 
-function BusPages({user, setUser}) {
+function BusPages({user, setUser, initials}) {
   return (
     <>
-      <NavBar2 user={user} setUser={setUser}/>
+      <NavBar2 user={user} setUser={setUser} initials={initials}/>
       <div className="space-y-24">
         <BusCard />
         <BusCard />
