54918ed0640a354e5fd35b9cd5212dc28791bc79
SheeTabz <wmuriithitabitha@gmail.com>
2023-01-19 09:29:27 +0300

    Add details

--- src/components/BookingCard.jsx
+++ src/components/BookingCard.jsx
@@ -11,7 +11,12 @@ function BookingCard() {
   return (
     <div ref={componentRef} >
   
-
+  <div className="text-center py-6">
+          <p className="text-xl font-medium text-gray-900">
+            CONGRATULATIONS On Your Successful <strong>Purchase !!!</strong>
+          </p>
+        </div>
+        
   <div class="cont">
   <div class="item shadow-xl">
     <div class="item-right">
@@ -27,13 +32,13 @@ function BookingCard() {
       
       <div class="sce">
        
-        <p>Gender:  Female</p>
+        <p>Gender: male</p>
       </div>
       <div class="fix"></div>
       <div class="loc">
       
         <p>Phone no:  0789076789</p>
-        <p>Route:  Nairobi - Garissa</p>
+        <p>Route:  Nairobi - Mombasa</p>
 
       </div>
       <div class="fix"></div>
